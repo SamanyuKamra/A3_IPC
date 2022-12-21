a087063da376228cffbfad6b4af406d3d06c2bcf
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-22 04:41:12 +0530

    Update sharedmem2.c

--- sharedmem2.c
+++ sharedmem2.c
@@ -1,23 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include<sys/shm.h>
+#include <semaphore.h>
 #include<time.h>
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<errno.h>
 #include<unistd.h>
-#include<math.h>
 #include<sys/ipc.h>
-#include<sys/shm.h>
-#include <semaphore.h>
+#include<math.h>
 
-
-#define index 8
-#define file "sem"
 struct timespec a1;
-#define num 50
 struct timespec a2;
+#define index 8
 
 int min(int x, int y)
 {
@@ -29,10 +26,7 @@ int min(int x, int y)
         return x;
     }
 }
-void leave(char** s)
-{
-    strcpy(*s,"waiting");
-}
+
 void capture(char** s)
 {
     while(strcmp(*s,"waiting")==0){}
@@ -44,18 +38,20 @@ int main()
     key_t passwd = ftok("SharedMemory",50);
     int id = shmget(passwd,1024,0666|IPC_CREAT);
     temp = (char*)shmat(id,NULL,0);
-    int var = 0;
-    while(var<num){
+    int var;
+    for(var=0;var<50;var = var+5)
+    {
         int b = var;
-        for(;b<min(var+5,num);b++){
+        for(;b<min(var+5,50);b++)
+        {
             capture(&temp);
             printf("received by p2 : %s\n",temp);
-            leave(&temp);
+            strcpy(*&temp,"waiting");
 
         }
-        var+=5;
     }
     clock_gettime(CLOCK_REALTIME,&a2);
     printf("Exexution time = %f\n",fabs(((a2.tv_sec - a1.tv_sec)+(a2.tv_nsec - a1.tv_nsec))/1e9));
     return 0;
+    
 }
