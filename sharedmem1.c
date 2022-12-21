fd3179f9e1937366fd1f6434f71efa6036333779
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-22 04:41:54 +0530

    Update sharedmem1.c

--- sharedmem1.c
+++ sharedmem1.c
@@ -1,20 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-#include<unistd.h>
-#include<sys/ipc.h>
 #include<sys/shm.h>
 #include <semaphore.h>
 #include<time.h>
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include<sys/ipc.h>
 
-
-#define num 50
 struct timespec a1;
-#define index 9
 
 int min(int x, int y)
 {
@@ -26,66 +23,50 @@ int min(int x, int y)
         return x;
     }
 }
-void leave(char** s)
-{
-    strcpy(*s,"");
-}
-void capture(char** s)
-{
-    while(strcmp(*s,"waiting")!=0){}
-}
 
 int main()
 {
-    
     clock_gettime(CLOCK_REALTIME,&a1);
-    char* arr[50];
-    int i;
-    for(i = 0;i<num;i++)
+    int i = 0;
+    int j = 0;
+
+    int random;
+    char str_2[5];
+    char arr[50][5];
+
+    for(i = 0; i<50; i++)
     {
-        arr[i] = (char*)malloc((index)*sizeof(char));
+        for(j=0; j<4; j++)
+        {
+            random = rand()%26 + 65;
+            str_2[j] = (char)(random);
+
+        }
+        str_2[5] = '\0';
+	    strcpy(arr[i], str_2);
+    }
+    printf("P1: The contents of the array are\n");
+    for (i = 0; i<50; i++){
+        printf("%s\n", arr[i]);
     }
 
-    for(int j = 0;j<num;j++)
-    {
-        int k;
-        if(j>=10)
-        {
-            arr[j][0] = '0'+j/10;
-            arr[j][1] = '0'+j%10;
-            arr[j][2] =' ';
-            k = 3;
-        }
-        else
-        {
-            arr[j][0] = '0' + j;
-            arr[j][1] = ' ';
-            k = 2;
-        }
-        while(k < index - 2)
-        {
-            arr[j][k] = rand()%26 + 65;
-            k++;
-        }
-        arr[j][index - 1] = '\0';
-    }
     char* temp = (char*)malloc(sizeof(arr[0]));
     key_t passwd = ftok("SharedMemory",50);
     int id = shmget(passwd,1024,0666|IPC_CREAT);
-
     temp = (char*) shmat(id,NULL,0);
-    
-    for(int a=0;a<num;)
+
+    for(int a=0;a<50;)
     {
         int d = a;
         while(d<min(a+5,50))
         {
             strcpy(temp,arr[d]);
-            capture(&temp);
+            while(strcmp(*&temp,"waiting")!=0){}
             d++;
         }
         a=d;
         printf("Maximum id received by p1: %d\n",a-1);
     }
     return 0;
+
 }
